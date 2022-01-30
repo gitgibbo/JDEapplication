a3cd531661f6004155ca682cd000a9014673ef6a
Gibbo <mattgibbons@me.com>
2022-01-30 20:01:20 +0000

    first commit

---
+++ bos/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.contrib.auth.models import Permission
+from import_export.admin import ImportExportModelAdmin
+from .models import BOS, Topic, Shift, Area
+#from permit_to_work.models import permit_to_work
+admin.site.register(Permission)
+
+@admin.register(BOS)
+class BOSadmin(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ['__str__', 'owner', 'Area']
+
+admin.site.register(Topic)
+admin.site.register(Shift)
+admin.site.register(Area)
+
+
+admin.site.site_header = "JDE application Admin"
+admin.site.site_title = "JDE App"
+admin.site.index_title = "Welcome to JDE App Portal"
---
+++ bos/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Topic(models.Model):
+    """Create a topic or risk for entry into BOS's"""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'Topics'
+
+    def __str__(self):
+        """Return string representation of model"""
+        return self.text
+
+class Shift(models.Model):
+    """Create a Shift for entry into BOS's"""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'Shifts'
+
+    def __str__(self):
+        """Return string representation of model"""
+        return self.text
+
+class Area(models.Model):
+    """Create an area for entry into BOS's"""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'Areas'
+
+    def __str__(self):
+        """Return string representation of model"""
+        return self.text
+
+class BOS(models.Model):
+    """The main structure of a BOS"""
+    
+    Area = models.ForeignKey(Area, on_delete=models.CASCADE)
+    Shift = models.ForeignKey(Shift, on_delete=models.CASCADE)
+    Topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    Comments = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    class Meta:
+        verbose_name_plural = 'BOS Conversations'
+    def __str__(self):
+        return ('BOS #' + str(self.id))
+
